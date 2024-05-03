2602ca36e155ccc4ed2aafd914dc4ca14ebebc42
etienne-onasch <eonasch@jastech-solutions.de>
2024-05-03 07:45:03 +0200

    change message

--- mongo-init.js
+++ mongo-init.js
@@ -3,5 +3,5 @@ var db = db.getSiblingDB('messages');
 db.createCollection('greetings');
 
 db.greetings.insertOne({
-  message: 'hello from server!'
+  message: 'hello from server! (this message was inserted from mongo-init.js)'
 });
